6c99a1da791108d5636be4bf1071b3b5f44ef53f
skt-t1-byungi <tiniwz@gmail.com>
2019-10-06 17:03:04 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import { useState, useRef, useMemo } from 'react'
+import pDefer, { Deferred } from '@byungi/p-defer'
+
+const STATE_BEFORE = 0 as const
+const STATE_PENDING = 1 as const
+const STATE_RESOLVED = 2 as const
+const STATE_REJECTED = 3 as const
+type STATE = 0|1|2|3
+
+export class ForceCancelError extends Error {
+    name = 'ForceCancelError'
+    isForceCanceled = true
+}
+
+interface UseDeferredOptions<Args extends [], Result> {
+    onExecute: (...args: Args) => void;
+    onResolve: (value: Result) => void;
+    onReject: (reason: any) => void;
+    onComplete: () => void;
+}
+
+export function useDeferred <Args extends [] = [], Result = any> (
+    {
+        onExecute = noop,
+        onResolve = noop,
+        onReject = noop,
+        onComplete = noop
+    }: Partial<UseDeferredOptions<Args, Result>> = {},
+    deps: any[] = []
+) {
+    const [state, setState] = useState<STATE>(STATE_BEFORE)
+    const deferRef = useRef<Deferred<Result>|null>(null)
+
+    const methods = useMemo(() => ({
+        execute (...args: Args) {
+            if (deferRef.current) return deferRef.current.promise
+            return methods.forceExecute(...args)
+        },
+
+        forceExecute (...args: Args) {
+            setState(STATE_PENDING)
+            onExecute(...args)
+
+            if (deferRef.current) {
+                deferRef.current.reject(new ForceCancelError('Canceled for a force execute.'))
+            }
+
+            return (deferRef.current = pDefer()).promise
+        },
+
+        resolve (res: Result) {
+            setState(STATE_RESOLVED)
+            onResolve(res)
+            onComplete()
+
+            if (deferRef.current) {
+                deferRef.current.resolve(res)
+                deferRef.current = null
+            }
+        },
+
+        reject (reason: any) {
+            setState(STATE_REJECTED)
+            onReject(reason)
+            onComplete()
+
+            if (deferRef.current) {
+                deferRef.current.reject(reason)
+                deferRef.current = null
+            }
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }), deps)
+
+    return {
+        isBefore: state === STATE_BEFORE,
+        isPending: state === STATE_PENDING,
+        isResolved: state === STATE_RESOLVED,
+        isRejected: state === STATE_REJECTED,
+        isComplete: state === STATE_RESOLVED || state === STATE_REJECTED,
+        ...methods
+    }
+}
+
+function noop () { }
